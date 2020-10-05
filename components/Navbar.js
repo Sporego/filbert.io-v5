9e8c6da8988c03edeccdc6bc61b0f7689fc49470
Sporego <contact@filbert.io>
2020-10-05 02:22:19 -0400

    fixed eslint proptype errors

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,6 +1,12 @@
+// import individual react-bootstrap components
 import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Form from 'react-bootstrap/Form';
+import FormControl from 'react-bootstrap/FormControl';
+import Button from 'react-bootstrap/Button';
 
-export default function Navbar() {
+export default function navbar() {
     // functions
 
     // returned component
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
 import '../styles/globals.css';
+import PropTypes from 'prop-types'; // React built-in typechecking https://reactjs.org/docs/typechecking-with-proptypes.html
 
 function MyApp({ Component, pageProps }) {
     return <Component {...pageProps} />;
 }
+
+MyApp.propTypes = {
+    Component: PropTypes.node,
+    pageProps: PropTypes.node
+};
 
 export default MyApp;
