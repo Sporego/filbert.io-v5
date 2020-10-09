2389880e75fd2e069aa977ef57058c33fcdf6430
Sporego <contact@filbert.io>
2020-10-09 15:07:45 -0400

    initial for branch

--- components/Navbar.js
+++ components/Navbar.js
@@ -12,13 +12,22 @@ export default function navbar() {
     // returned component
     return (
         <Navbar bg="dark" expand="lg" variant="dark">
-            <Navbar.Brand href="#home">Filbert.IO</Navbar.Brand>
+            <Navbar.Brand href="#home">
+                <img
+                    alt="logo"
+                    src="C:/Users/conta/Downloads/html-20s-100px.svg"
+                    width="30"
+                    height="30"
+                    className="d-inline-block align-top"
+                />{' '}
+                Filbert.IO
+            </Navbar.Brand>{' '}
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto">
                     <Nav.Link href="#home">About</Nav.Link>
                     <Nav.Link href="#link">Blog</Nav.Link>
-                    <NavDropdown title="Demos" id="basic-nav-dropdown">
+                    <NavDropdown title="Tech Demos" id="basic-nav-dropdown">
                         <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                         <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
                         <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
