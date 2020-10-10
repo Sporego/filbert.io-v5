426ef32f0b2dc01beb5d7102625c992e089afcac
Sporego <contact@filbert.io>
2020-10-09 21:51:41 -0400

    Jumbotron added

---
+++ components/Jumbo.js
@@ -0,0 +1,20 @@
+// import individual react-bootstrap components
+import Jumbotron from 'react-bootstrap/Jumbotron'
+
+export default function navbar() {
+    // functions
+
+    // returned component
+    return (
+<Jumbotron>
+  <h1>Hello, world!</h1>
+  <p>
+    This is a simple hero unit, a simple jumbotron-style component for calling
+    extra attention to featured content or information.
+  </p>
+  <p>
+    <Button variant="primary">Learn more</Button>
+  </p>
+</Jumbotron>
+    );
+}
